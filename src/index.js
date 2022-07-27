08a8e981fbac8371872718a8e04abd5baca55ca4
Jana Ahurtsova <byjankaaa@gmail.com>
2022-07-27 12:47:49 +0300

    feat: add solving to task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let stack = [];
+  let openingBrackets = [];
+  const bracketPairs = Object.fromEntries(bracketsConfig);
+
+  if(str.length % 2 != 0) {
+    return false;
+  }
+
+  for (let j= 0; j < bracketsConfig.length; j++) {
+    openingBrackets.push(bracketsConfig[j][0])
+  }
+
+  for (let i=0; i < str.length; i++) {
+    if(openingBrackets.includes(str[i])) {
+
+      if(str[i] === bracketPairs[str[i]] && str[i] === stack[stack.length - 1]) {
+        stack.pop();
+        continue;
+      }
+
+      stack.push(str[i]);
+      continue;
+    } 
+
+    if (str[i] === bracketPairs[stack[stack.length - 1]]) {
+    stack.pop();
+    } 
+  }
+  return stack.length === 0;
+
+  };
+
+
